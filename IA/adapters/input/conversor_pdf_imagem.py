11f80dbe01d358c4f82bab91318fe1189ecd5944
PeLisboa <plisboa2003@gmail.com>
2025-05-31 18:58:46 -0300

    Função conversão PDF - Imagem

--- IA/adapters/input/conversor_pdf_imagem.py
+++ IA/adapters/input/conversor_pdf_imagem.py
@@ -6,4 +6,17 @@ class ConversorPDFImagem:
     def __init__(self, poppler_path:str):
         self.poppler_path = poppler_path
         
-    
+    def converter_2_imagens(self, caminho_pdf: str, pasta_destino: str) -> List[str]:
+        #Criação da pasta destino
+        os.makedirs(pasta_destino, exist_ok = True)
+        self._limpar_pasta(pasta_destino)
+        
+        imagens = convert_from_path(caminho_pdf, poppler_path = self.poppler_path)
+        caminhos_imagens = []
+        
+        for i, imagem in enumerate(imagens):
+            caminho_imagem = os.path.join(pasta_destino, f"pagina_{i + 1}.png")
+            imagem.save(caminho_imagem, "PNG")
+            caminhos_imagens.append(caminho_imagem)
+            
+        return caminhos_imagens
