fe97f3858abaa2111171270eb35288ccb2d44de9
PeLisboa <plisboa2003@gmail.com>
2025-05-31 20:01:28 -0300

    Função: Extração de texto com OCR

--- IA/application/services/ocr_service.py
+++ IA/application/services/ocr_service.py
@@ -10,4 +10,16 @@ class OCRService:
         if teseract_cmd:
             pytesseract.pytesseract.tesseract_cmd = teseract_cmd
         
-    
+    def extrair_texto(self, pasta_imagens: str) -> Dict[str, str]:
+        
+        textos = {}
+        
+        for i, nome_arquivo in enumerate(sorted(os.listdir(pasta_imagens))):
+            caminho_imagem = os.path.join(pasta_imagens, nome_arquivo)
+            
+            if os.path.isfile(caminho_imagem):
+                imagem = Image.open(caminho_imagem)
+                texto = pytesseract.image_to_string(imagem)
+                textos[f"texto_{i + 1}"] = texto
+                
+        return textos
