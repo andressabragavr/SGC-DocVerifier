8fd52dfe0c26a3920f9bec387c637c9cb5867ed9
PeLisboa <plisboa2003@gmail.com>
2025-06-01 01:54:44 -0300

    Chamado da OpenAI na main

--- IA/main.py
+++ IA/main.py
@@ -8,6 +8,7 @@ from resources.prompts.carregador_prompt import carregar_prompt_formatado
 from resources.atividades import atividades
 from adapters.llm.gemini_adapter import GeminiAdapter
 from application.services.llm_service import LLMService
+from adapters.llm.openai_adapter import OpenAIAdapter
 
 if __name__ == "__main__":
     load_dotenv()
@@ -52,8 +53,12 @@ if __name__ == "__main__":
     )
     
     # Gemini
-    gemini_api_key = os.getenv("GEMINI_API_KEY")
-    adapter = GeminiAdapter(api_key=gemini_api_key)
+    # gemini_api_key = os.getenv("GEMINI_API_KEY")
+    # adapter = GeminiAdapter(api_key=gemini_api_key)
+    
+    # OpenAI
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    adapter = OpenAIAdapter(api_key=openai_api_key)
     
     servico_llm = LLMService(adapter)
     resposta = servico_llm.obter_resposta(prompt_formatado, contexto=contexto)
