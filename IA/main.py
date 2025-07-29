e383653055b597cadb1eff02f0cfc8c435e2afc1
PeLisboa <plisboa2003@gmail.com>
2025-07-29 09:50:28 -0300

    Ajustes na main para rodar em conjunto com as outras camadas

--- IA/main.py
+++ IA/main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import json
 from dotenv import load_dotenv
 from application.services.detector_tipo_service import DetectorTipoService
 from adapters.input.conversor_pdf_imagem import ConversorPDFImagem
@@ -6,7 +8,7 @@ from application.services.ocr_service import OCRService
 from application.services.rag_service import contexto_rag
 from resources.prompts.carregador_prompt import carregar_prompt_formatado
 from resources.atividades import atividades
-from adapters.llm.gemini_adapter import GeminiAdapter
+# from adapters.llm.gemini_adapter import GeminiAdapter
 from application.services.llm_service import LLMService
 from adapters.llm.openai_adapter import OpenAIAdapter 
 from application.services.json_service import salvar_resultado_em_json
@@ -14,16 +16,29 @@ from application.services.json_service import salvar_resultado_em_json
 if __name__ == "__main__":
     load_dotenv()
     
-    pasta_imagens = "data/imagens_convertidas/"
+    pasta_imagens = "../IA/data/imagens_convertidas"
     
     # Pedro
-    caminho_arquivo = "data/alunos/certificados_Pedro_Lisboa.pdf"
-    nome_aluno = "Pedro Henrique Lisboa"
+    # caminho_arquivo = "data/alunos/certificados_Pedro_Lisboa.pdf"
+    # nome_aluno = "Pedro Henrique Lisboa"
     
     # Andressa
     # caminho_arquivo = "data/alunos/210058 - Andressa Braga.pdf"
     # caminho_arquivo = "data/alunos/CursoHTML, CSS e Boostrap5 -ANDRESSA.pdf"
     # nome_aluno = "Andressa Braga Vieira Rodrigues"
+    
+    # RECEBIMENTO DE ARGUMENTOS DO BACKEND
+    if len(sys.argv) < 3:
+        print(json.dumps({"erro": "Parâmetros insuficientes: <caminho_arquivo> <nome_aluno>"}))
+        sys.exit(1)
+
+    caminho_arquivo = sys.argv[1]
+    nome_aluno = sys.argv[2]
+
+    # VERIFICAÇÃO DE EXISTÊNCIA
+    if not os.path.exists(caminho_arquivo):
+        print(json.dumps({"erro": f"Arquivo não encontrado: {caminho_arquivo}"}))
+        sys.exit(1)
     
     poppler_path = r"C:\Arquivos de Programas\poppler\poppler-24.08.0\Library\bin"
     tesseract_path = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -51,8 +66,10 @@ if __name__ == "__main__":
     contexto = contexto_rag(texto_completo, query = "Extração de informações do certificado")
         
     # Prompt
+    prompt_path = os.path.join(os.path.dirname(__file__), "resources", "prompts", "base_prompt.txt")
+    
     prompt_formatado = carregar_prompt_formatado(
-        "resources/prompts/base_prompt.txt",
+        prompt_path,
         texto_dict = textos_extraidos,
         num_cert = num_cert,
         nome_aluno = nome_aluno,
@@ -61,16 +78,16 @@ if __name__ == "__main__":
     )
     
     # Gemini
-    gemini_api_key = os.getenv("GEMINI_API_KEY")
-    adapter = GeminiAdapter(api_key=gemini_api_key)
+    # gemini_api_key = os.getenv("GEMINI_API_KEY")
+    # adapter = GeminiAdapter(api_key=gemini_api_key)
     
     # OpenAI
-    # openai_api_key = os.getenv("OPENAI_API_KEY")
-    # adapter = OpenAIAdapter(api_key=openai_api_key)
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    adapter = OpenAIAdapter(api_key=openai_api_key)
     
     servico_llm = LLMService(adapter)
     resposta = servico_llm.obter_resposta(prompt_formatado, contexto=contexto)
     
     arquivo = salvar_resultado_em_json(resposta, "resultado_certificado")
     
-    print("Finalizado")
+    print(resposta)
